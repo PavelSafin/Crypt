a977886ba2f5763695cec8b3f3d73dddc29566f1
PavelSafin <pavel_safin@icloud.com>
2016-07-06 22:49:08 +0300

    Rewritten to work on classes

--- DES/main.cpp
+++ DES/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <sstream>
+#include "DES.h"
 
 int main() {
-    unsigned long long key;
-    std::string message;
+    string key;
+    string message;
 
     std::cout << "Please enter message" << std::endl;
     std::cin >> message;
@@ -10,9 +12,12 @@ int main() {
     std::cout << "Please enter key (64-bit number)" << std::endl;
     std::cin >> key;
 
-    if (key < ULLONG_MAX && key > 0) {
-        //Do something
-    } else {
-        std :: cout << "Invalid number" << std::endl;
-    }
+//    if (key < ULLONG_MAX && key > 0) {
+        DES des(key);
+
+        des.set_message(message);
+        cout << des.encode_message() << endl;
+//    } else {
+//        std :: cout << "Invalid number" << std::endl;
+//    }
 }
